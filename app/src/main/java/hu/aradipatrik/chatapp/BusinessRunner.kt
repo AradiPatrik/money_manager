f4faa7ce648884fcc51c59d80833fecc25c808e9
Aradi Patrik <aradipatrik@gmail.com>
2019-07-14 11:58:05 +0200

    last commit added viewmodel and livedata too

--- app/src/main/java/hu/aradipatrik/chatapp/BusinessRunner.kt
+++ app/src/main/java/hu/aradipatrik/chatapp/BusinessRunner.kt
@@ -5,6 +5,7 @@ import javax.inject.Inject
 
 class BusinessRunner @Inject constructor(private val businessUtil: BusinessUtil) {
     fun runBusiness() {
+
         Log.d(this::class.java.simpleName, "runningBusiness")
         businessUtil.doBusiness()
     }
