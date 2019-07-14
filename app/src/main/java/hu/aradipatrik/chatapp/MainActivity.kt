037a348ccb07d38d22bf4abc4433087fe4dfb724
Aradi Patrik <aradipatrik@gmail.com>
2019-07-14 11:39:23 +0200

    enable databinding

--- app/src/main/java/hu/aradipatrik/chatapp/MainActivity.kt
+++ app/src/main/java/hu/aradipatrik/chatapp/MainActivity.kt
@@ -2,11 +2,14 @@ package hu.aradipatrik.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import hu.aradipatrik.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.welcomeText = "Databinding Works!"
     }
 }
