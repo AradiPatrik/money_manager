9b179f5ed53242b1a48394c1f1ada8edb01f1497
Aradi Patrik <aradipatrik@gmail.com>
2019-12-24 14:56:33 +0100

    define transaction data source interface

--- Data/src/main/java/com/aradipatrik/data/repository/TransactionsDataSource.kt
+++ Data/src/main/java/com/aradipatrik/data/repository/TransactionsDataSource.kt
@@ -5,14 +5,10 @@ import io.reactivex.Completable
 import io.reactivex.Observable
 import org.joda.time.Interval
 
-
-interface TransactionsLocal {
+interface TransactionsDataSource {
     fun getAllTransactions(): Observable<List<TransactionEntity>>
     fun getTransactionsInInterval(interval: Interval): Observable<List<TransactionEntity>>
+    fun deleteTransactions(id: String): Completable
     fun addTransaction(transaction: TransactionEntity): Completable
-    fun updateTransaction(transaction: TransactionEntity): Completable
-    fun deleteTransaction(id: String): Completable
-
-    fun getUnsynchronisedTransactions(): Observable<List<TransactionEntity>>
-    fun setSynchronised(ids: List<String>): Completable
+    fun editTransaction(transaction: TransactionEntity): Completable
 }
--- Data/src/main/java/com/aradipatrik/data/repository/TransactionsRemote.kt
+++ Data/src/main/java/com/aradipatrik/data/repository/TransactionsRemote.kt
@@ -2,9 +2,10 @@ package com.aradipatrik.data.repository
 
 import com.aradipatrik.data.model.TransactionEntity
 import io.reactivex.Completable
+import io.reactivex.Observable
 
 interface TransactionsRemote {
-    fun getAllTransactions(): List<TransactionEntity>
-    fun getTransactionsAfter(timestamp: Long): List<TransactionEntity>
+    fun getAllTransactions(): Observable<List<TransactionEntity>>
+    fun getTransactionsAfter(timestamp: Long): Observable<List<TransactionEntity>>
     fun updateWithOfflineTransactions(transactions: List<TransactionEntity>): Completable
 }
