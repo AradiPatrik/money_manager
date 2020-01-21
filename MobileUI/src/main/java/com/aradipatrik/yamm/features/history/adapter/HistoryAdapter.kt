53ecb3c7874c85ddd35a0a1dd7d2e0ea30dd094e
Aradi Patrik <aradipatrik@gmail.com>
2020-01-21 21:36:23 +0100

    finish category adapter

--- MobileUI/src/main/java/com/aradipatrik/yamm/features/history/adapter/HistoryAdapter.kt
+++ MobileUI/src/main/java/com/aradipatrik/yamm/features/history/adapter/HistoryAdapter.kt
@@ -1,14 +1,19 @@
 package com.aradipatrik.yamm.features.history.adapter
 
 import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.aradipatrik.yamm.R
+import com.aradipatrik.yamm.features.history.adapter.HistoryAdapter.ViewHolder.TransactionHeaderViewHolder
+import com.aradipatrik.yamm.features.history.adapter.HistoryAdapter.ViewHolder.TransactionItemViewHolder
 import com.aradipatrik.yamm.features.history.model.TransactionHeaderViewData
 import com.aradipatrik.yamm.features.history.model.TransactionItemViewData
 import com.aradipatrik.yamm.features.history.model.TransactionViewData
+import kotlinx.android.synthetic.main.list_item_history_header.view.*
 import kotlinx.android.synthetic.main.list_item_history_transaction.view.*
 
 object TransactionViewDataItemCallback : DiffUtil.ItemCallback<TransactionViewData>() {
@@ -51,26 +56,50 @@ class HistoryAdapter
         is TransactionItemViewData -> TRANSACTION_ITEM
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder = when(viewType) {
+        TRANSACTION_HEADER -> TransactionHeaderViewHolder(
+            parent.inflate(R.layout.list_item_history_header)
+        )
+        TRANSACTION_ITEM -> TransactionItemViewHolder(
+            parent.inflate(R.layout.list_item_history_transaction)
+        )
+        else -> error("Invalid type requested ($viewType) as transaction list item")
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = when(holder) {
+        is TransactionItemViewHolder ->
+            holder.bind(currentList[position] as TransactionItemViewData)
+        is TransactionHeaderViewHolder ->
+            holder.bind(currentList[position] as TransactionHeaderViewData)
     }
 
     sealed class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         class TransactionItemViewHolder(view: View) : ViewHolder(view) {
-            private val circularBackground = view.circular_background
             private val categoryIcon = view.category_icon
             private val categoryName = view.category_name
             private val memo = view.memo
             private val amount = view.amount
 
             fun bind(transactionItemViewData: TransactionItemViewData) {
-
+                categoryIcon.setImageResource(transactionItemViewData.categoryIconResId)
+                categoryName.text = transactionItemViewData.categoryName
+                memo.text = transactionItemViewData.memo
+                amount.text = transactionItemViewData.amount.toString()
             }
         }
-        class HeaderItemViewHolder(view: View) : ViewHolder(view)
+
+        class TransactionHeaderViewHolder(view: View) : ViewHolder(view) {
+            private val dateHeader = view.date_header
+
+            fun bind(transactionHeaderViewData: TransactionHeaderViewData) {
+                dateHeader.text = transactionHeaderViewData.asFormattedString
+            }
+        }
     }
 }
+
+private fun ViewGroup.inflate(layoutResource: Int) = LayoutInflater.from(context)
+    .inflate(layoutResource, this, false)
