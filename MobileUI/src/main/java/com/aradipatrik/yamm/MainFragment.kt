439a2923ae854770c309ebe56b492646762d3fbe
Aradi Patrik <aradipatrik@gmail.com>
2020-02-13 18:58:10 +0100

    wipwipwip

--- MobileUI/src/main/java/com/aradipatrik/yamm/MainFragment.kt
+++ MobileUI/src/main/java/com/aradipatrik/yamm/MainFragment.kt
@@ -5,8 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.airbnb.mvrx.BaseMvRxFragment
+import com.aradipatrik.yamm.MainFragmentUiEffect.FabClick
+import com.aradipatrik.yamm.common.viewext.hideAsBottomSheet
+import com.aradipatrik.yamm.common.viewext.showAsBottomSheet
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.jakewharton.rxbinding3.view.clicks
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
+import kotlinx.android.synthetic.main.fragment_main.*
+
+sealed class MainFragmentUiEffect {
+    object FabClick: MainFragmentUiEffect()
+}
 
 class MainFragment : BaseMvRxFragment() {
+    private val uiEffectsDisposable = CompositeDisposable()
+
+    private val uiEffects get() = fab.clicks().map { FabClick }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,10 +31,34 @@ class MainFragment : BaseMvRxFragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun onResume() {
+        super.onResume()
+//        uiEffectsDisposable += uiEffects.subscribe(::handleUiEffect)
     }
+
+    override fun onPause() {
+        super.onPause()
+        uiEffectsDisposable.clear()
+    }
+
+//    private fun handleUiEffect(effect: MainFragmentUiEffect) = when(effect) {
+//        FabClick -> TODO()
+//    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+//            hideCalculator()
+            hideSumSheet()
+        }
+    }
+
+//    private fun showCalculator() = calculator_sheet.showAsBottomSheet()
+//
+//    private fun hideCalculator() = calculator_sheet.hideAsBottomSheet()
+
+    private fun showSumSheet() = sum_sheet_container.showAsBottomSheet()
+
+    private fun hideSumSheet() = sum_sheet_container.hideAsBottomSheet()
 
     override fun invalidate()  {
 
--- MobileUI/src/main/java/com/aradipatrik/yamm/common/viewext/ViewExtensions.kt
+++ MobileUI/src/main/java/com/aradipatrik/yamm/common/viewext/ViewExtensions.kt
@@ -1,7 +1,17 @@
 package com.aradipatrik.yamm.common.viewext
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 fun ViewGroup.inflate(layoutResource: Int) = LayoutInflater.from(context)
     .inflate(layoutResource, this, false)
+
+fun View.hideAsBottomSheet() {
+    BottomSheetBehavior.from(this).state = BottomSheetBehavior.STATE_HIDDEN
+}
+
+fun View.showAsBottomSheet() {
+    BottomSheetBehavior.from(this).state = BottomSheetBehavior.STATE_EXPANDED
+}
