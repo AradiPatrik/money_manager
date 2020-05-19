6376569ccc1cb6062cf1064e779ffcac91c34ec8
Patrik Aradi <patrik.aradi@supercharge.io>
2020-05-19 07:19:08 +0200

    start work on sumsheet

--- Presentation/src/main/java/com/aradipatrik/presentation/viewmodels/sumsheet/SumSheetViewModel.kt
+++ Presentation/src/main/java/com/aradipatrik/presentation/viewmodels/sumsheet/SumSheetViewModel.kt
@@ -1,18 +1,45 @@
 package com.aradipatrik.presentation.viewmodels.sumsheet
 
 import com.airbnb.mvrx.Async
+import com.airbnb.mvrx.MvRxState
+import com.airbnb.mvrx.Uninitialized
+import com.aradipatrik.domain.interactor.selectedmonth.DecrementSelectedMonthInteractor
+import com.aradipatrik.domain.interactor.selectedmonth.GetSelectedMonthInteractor
+import com.aradipatrik.domain.interactor.selectedmonth.IncrementSelectedMonthInteractor
+import com.aradipatrik.domain.interactor.transaction.GetTransactionsInIntervalInteractor
 import com.aradipatrik.domain.model.Transaction
-import org.joda.time.DateTime
+import com.aradipatrik.presentation.common.MvRxViewModel
+import org.joda.time.Interval
+import org.joda.time.YearMonth
 
-data class SplashState(
-    val selectedMonthOperation: Async<DateTime>,
-    val transactionsInSelectedMonthOperation: Async<List<Transaction>>,
-    val incomeThisMonth: Int?,
-    val expenseThisMonth: Int?,
-    val grandTotal: Int?,
-    val monthlyTotal: Int?
-)
+data class SumSheetState(
+    val getTransactionsOperation: Async<List<Transaction>> = Uninitialized,
+    val incomeThisMonth: Int? = null,
+    val expenseThisMonth: Int? = null,
+    val grandTotal: Int? = null,
+    val monthlyTotal: Int? = null
+) : MvRxState
 
-class SumSheetViewModel {
-    
+class SumSheetViewModel(
+    initialState: SumSheetState,
+    getSelectedMonthInteractor: GetSelectedMonthInteractor,
+    incrementSelectedMonthInteractor: IncrementSelectedMonthInteractor,
+    decrementSelectedMonthInteractor: DecrementSelectedMonthInteractor,
+    getTransactionsInIntervalInteractor: GetTransactionsInIntervalInteractor
+) : MvRxViewModel<SumSheetState>(initialState) {
+    init {
+        getSelectedMonthInteractor.get()
+            .map { Interval(YearMonth(it).toInterval()) }
+            .switchMap { interval ->
+                getTransactionsInIntervalInteractor.get(
+                    GetTransactionsInIntervalInteractor.Params.forInterval(interval)
+                ).map { transactions -> transactions to interval}
+            }
+            .execute {
+                copy(
+
+                )
+            }
+
+    }
 }
